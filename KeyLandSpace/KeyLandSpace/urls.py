46f4a8fc1710cab874a4c6c21a12e18cb46f4c87
Pratima121 <58822379+Pratima121@users.noreply.github.com>
2020-01-24 13:16:59 +0545

    login,signup,session

--- KeyLandSpace/KeyLandSpace/urls.py
+++ KeyLandSpace/KeyLandSpace/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from products.views import view_authenticate_user
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('products/',include('products.urls'))
+]
+
+urlpatterns += [
+    # path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/login/',view_authenticate_user)
 ]
