cb90fa4a20c797e7a5b2e696203d7b10132940da
Chorten-Tsomo <58814995+Chorten-Tsomo@users.noreply.github.com>
2020-02-09 22:25:31 +0545

    ModelRelationship Created

--- KeyLandSpace/ModelRelationship/models.py
+++ KeyLandSpace/ModelRelationship/models.py
@@ -2,36 +2,66 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Customer(models.Model):
-    Customer_Id = models.IntegerField(primary_key=True)
-    First_Name = models.CharField(max_length=50)
-    Last_Name = models.CharField(max_length=50)
+class Buyer(models.Model):
+    Name = models.CharField(max_length=50)
     Email = models.EmailField()
 
     def __str__(self):
-        return f"{self.Customer_Id} {self.First_Name} {self.Last_Name} {self.Email}"
+        return f"{self.Name} {self.Email}"
+    
+    
+
+class Seller(models.Model):
+    Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+
+
+    
+    def __str__(self):
+        return f"{self.Name} {self.Email}"
+
+    
+
+class Customer(models.Model):
+    First_Name = models.CharField(max_length=50)
+    Last_Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    buyer = models.ForeignKey(Buyer,on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller,on_delete=models.CASCADE)
+   
+
+    def __str__(self):
+        return f"{self.First_Name} {self.Last_Name} email id is {self.Email} name of buyer is {self.buyer} and seller is {self.seller}"
+
+    
+    
 
 
 class Product(models.Model):
     Name = models.CharField(max_length=50)
     Category = models.CharField(max_length=50)
     Condition = models.CharField(max_length=50)
-    Price = models.IntegerField()
-    Quantity = models.ManyToManyField(Customer,blank=True,related_name='customers')
-    
-    
-    
+    Price = models.FloatField()
+   
     def __str__(self):
-        return f"From{self.Name} {self.Category} {self.Condition} {self.Price} {self.Quantity}"
+        return f"{self.Name} falls on {self.Category} it is {self.Condition} costing {self.Price}"
 
+
+class Order_items(models.Model):
+    products = models.ForeignKey(Product,on_delete=models.CASCADE)
+    description = models.TextField()
+   
+    def __str__(self):
+        return f"{self.products.Name} is {self.description}"
+ 
 
 
 class Order(models.Model):
-    Order_Id = models.IntegerField(primary_key=True)
-    Status = models.ForeignKey(Customer,on_delete=models.CASCADE,related_name="productstatus")
-
+    customer = models.ManyToManyField(User,blank=True)
+    product = models.ManyToManyField(Order_items)
+    ordered_date = models.DateTimeField()
     
-    
-
     def __str__(self):
-        return f"{self.Order_Id} {self.Status} "
+        return f" {list(self.customer.all())} ordered {list(self.product.all())} in{self.ordered_date}"
+        
+    
